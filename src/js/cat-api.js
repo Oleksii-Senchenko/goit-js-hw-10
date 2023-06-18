838c575e844731c23d450478fdb8db5f79013ca5
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-06-18 14:08:11 +0300

    chek for fix

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,16 +1,6 @@
 import refs from './refs'
 const API_KEY = 'live_x2glOrtXN65yWxc6hIzm1fLtDFAD9O4CczTDA4QkUvYD1d6rgUneCd4yRde5eNue'
 
-// function load() {
-//     if (!fetchBreeds) {
-
-//         return
-//     }
-// }
-
-
-
-
 refs.errorEl.style.display = 'none'
 // const errMessage = refs.errorEl.display = 'block'
 export function fetchBreeds() {
@@ -29,7 +19,9 @@ export function fetchBreeds() {
 fetchBreeds().then((data) => {
     const options = data.map((el) => {
 
+
         return `<option value="${el.id}">${el.name}</option>`
+
     }).join('')
     refs.select.insertAdjacentHTML('afterbegin', options)
 }).catch((err) => {
@@ -60,16 +52,43 @@ export function fetchCatByBreed(breedId) {
 
 refs.select.addEventListener('change', e => {
     e.preventDefault()
+
+
+
+
+
     fetchCatByBreed(refs.select.value).then(data => {
         refs.catInfo.textContent = ''
-        data.map((el) => {
-            console.log(data);
+        // ЭТОТ МЕТОД РАБОЧИЙ
+        // data.map((el) => {
+            // 
 
-            const render = `<img src="${el.url}" alt="" width="450" height="300"></img>`
-            // const render2 = ` <h1>${el.breeds.name}</h1>`
-            refs.catInfo.insertAdjacentHTML('afterbegin', render)
-            refs.bodyel.insertAdjacentHTML('afterbegin', render2)
-           
-        })
+        //     const render = `<img src="${el.url}" alt="" width="450" height="300"></img>`
+
+        //     refs.catInfo.insertAdjacentHTML('afterbegin', render)
+
+        //     console.log(data.breeds[0]); ЭТОТ ЛОГ НЕ РАБОТАЕТ
+
+
+        // })
+
+        function careateMurkap(data) {
+            const img = data.url
+
+            const { name, description, temperament } = data.breeds[0]
+            return `
+            <img src="${img}" alt="${name}">
+            <div>
+            <h2>${name}</h2>
+            <p>${description}</p>
+            <p><span>${temperament}</span></p>
+          </div>`
+        }
+        console.log(nema);
+        careateMurkap(data)
+    }).catch((err) => {
+
     })
 })
+
+
--- src/js/refs.js
+++ src/js/refs.js
@@ -3,6 +3,7 @@ const refs = {
     catInfo: document.querySelector('.cat-info'),
     loaderEl: document.querySelector('.loader'),
     errorEl: document.querySelector('.error'),
-    bodyel: document.querySelector('body')
+    bodyel: document.querySelector('body'),
+    h1: document.querySelector('h1')
 }
 export default refs
